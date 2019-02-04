29b3bce4ae227c6bb2fb271a7737711a6e5a0062
gk <gk@172.16.2.56>
2019-02-04 15:08:41 +0100

    first version

---
+++ startGBDynConf/src/main/java/com/wha/springmvc/model/Conseiller.java
@@ -0,0 +1,48 @@
+package com.wha.springmvc.model;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.*;
+
+@Entity
+@DiscriminatorValue("utilisateur")
+public class Conseiller extends User implements Serializable {
+
+	private int matricule;
+	private Date DateDebutContrat;
+	
+	public Conseiller() {
+		super();
+		
+	}
+	
+
+	public Conseiller(int matricule, Date dateDebutContrat) {
+		this.matricule = matricule;
+		DateDebutContrat = dateDebutContrat;
+	}
+
+
+	public int getMatricule() {
+		return matricule;
+	}
+
+
+	public void setMatricule(int matricule) {
+		this.matricule = matricule;
+	}
+
+
+	public Date getDateDebutContrat() {
+		return DateDebutContrat;
+	}
+
+
+	public void setDateDebutContrat(Date dateDebutContrat) {
+		DateDebutContrat = dateDebutContrat;
+	}
+	
+	
+	
+	
+}
