f781f85f66684cc3fd320dbcf753556f9f6aa7d7
gk <gk@172.16.2.54>
2019-02-05 14:15:19 +0100

    Push dao compte impl

---
+++ startGBDynConf/src/main/java/com/wha/springmvc/dao/CompteDaoImpl.java
@@ -0,0 +1,45 @@
+package com.wha.springmvc.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import com.wha.springmvc.model.Compte;
+
+public class CompteDaoImpl implements ICompteDAO {
+	
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	@Override
+	public void addCompte(Compte c) {
+
+		entityManager.persist(c);
+		
+	}
+
+	@Override
+	public List<Compte> listCompte() {
+
+		Query req=entityManager.createQuery("select c from Compte c");
+		return req.getResultList();
+	}
+
+	@Override
+	public void deleteCompte(Long id) {
+
+		Compte c = entityManager.find(Compte.class, id);
+		entityManager.remove(c);
+		
+	}
+
+	@Override
+	public void updateCompte(Compte c) {
+
+		entityManager.merge(c);
+		
+	}
+
+}
