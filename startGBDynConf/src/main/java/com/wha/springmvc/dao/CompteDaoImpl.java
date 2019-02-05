5e4d14439049183e6d9a7034effe5a6bb4a2294b
gk <gk@172.16.2.54>
2019-02-05 14:59:03 +0100

    Compte Dao annot

--- startGBDynConf/src/main/java/com/wha/springmvc/dao/CompteDaoImpl.java
+++ startGBDynConf/src/main/java/com/wha/springmvc/dao/CompteDaoImpl.java
@@ -5,9 +5,13 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Repository;
 
 import com.wha.springmvc.model.Compte;
-
+@Repository
+@Transactional
 public class CompteDaoImpl implements ICompteDAO {
 	
 	@PersistenceContext
